d07ae52d18bbafa13f8e69ddffe9f5a0ba3a2fba
muhammadalvin92 <alvinbomber87@gmail.com>
2023-11-08 21:50:02 +0700

    setup template dan navbar

---
+++ resources/views/Pages/Home.blade.php
@@ -0,0 +1,6 @@
+@extends('Template')
+@section('title_tab', 'Home')
+<link rel="stylesheet" href="../Css/Home.css">
+@section('Content')
+    <h1>haiii</h1>
+@endsection
---
+++ resources/views/Parts/Navbar.blade.php
@@ -0,0 +1,44 @@
+<link rel="stylesheet" href="../Css/Navbar.css">
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ABOUT US</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">MICE</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">CONSULTANT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">FASILITATOR</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">EVENT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">GALLERY</a>
+          </li>
+          {{-- <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown link
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li> --}}
+        </ul>
+      </div>
+    </div>
+  </nav>
