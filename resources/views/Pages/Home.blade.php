01d3f43e2be2031e0ff8db297c8957eeaeaa53b9
muhammadalvin92 <alvinbomber87@gmail.com>
2023-11-13 11:21:22 +0700

    Update

--- resources/views/Pages/Home.blade.php
+++ resources/views/Pages/Home.blade.php
@@ -257,7 +257,7 @@
                     <img src="{{asset('../image/pdfiLogoEllipse.png')}}" alt="">
             </div>
             <div class="col-lg-3 col-md-3 footerTitle">
-                <p class="xl fw-bold my-2">PERSATUAN DOKTER FORENSIK INDONESIA</p>
+                <p class="xl fw-bold my-2">RAZA DIPTA PRATAMA</p>
                 <p class="text-justify my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero sapiente alias nam laborum? At tempora dolores architecto eligendi.</p>
                 <div class="sosmed d-flex align-items-center justify-content-center justify-content-md-start gap-3 my-3">
                     <a href=""><i class="fa-brands fa-facebook-f" style="color: #ffffff;"></i></a>
@@ -268,15 +268,21 @@
                     <a href=""><i class="fa-solid fa-circle-play" style="color: #ffffff;"></i></a>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-3 col-sm-8 col-7">
-                <p class="xl fw-bold my-2">ALAMAT</p>
-                <p class="text-justify my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci similique earum odit iste quo cum incidunt inventore! Saepe.</p>
+            <div class="col-lg-2 col-md-3 col-sm-8 col-7">
+                <p class="xl fw-bold my-2">MENU</p>
+                <a class="d-block text-decoration-none text-white" href="">Home</a>
+                <a class="d-block text-decoration-none text-white" href="">About Us</a>
+                <a class="d-block text-decoration-none text-white" href="">Mice</a>
+                <a class="d-block text-decoration-none text-white" href="">Consultant</a>
+                <a class="d-block text-decoration-none text-white" href="">Fasilitator</a>
+                <a class="d-block text-decoration-none text-white" href="">Event</a>
+                <a class="d-block text-decoration-none text-white" href="">Gallery</a>
             </div>
-            <div class="col-lg-2 col-md-3 col-sm-4 col-5">
-                <p class="xl fw-bold mt-2">KONTAK KAMI</p>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-5">
+                <p class="xl fw-bold mt-2">Addres & Contact</p>
                 <div class="email d-flex align-items-center gap-2 mt-2">
                     <i class="fa-solid fa-envelope" style="color: #ffffff;"></i>
-                    <p>pdfi@gmail.co.id</p>
+                    <p>PT Raza dipta pratama</p>
                 </div>
                 <div class="email d-flex align-items-center gap-2">
                     <i class="fa-brands fa-whatsapp" style="color: #ffffff;"></i>
@@ -286,7 +292,7 @@
         </div>
         <div class="hr"></div>
         <div class="bottomFooter d-flex justify-content-between">
-            <p>©2020 PDFI, All Rights Reserved.</p>
+            <p>©2020 PT Raza Dipta Pratama, All Rights Reserved.</p>
             <p>Terms & Condition - Privacy - Cookies</p>
         </div>
     </div>
